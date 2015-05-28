61f8e394ec8b555b78d021319557b7db15da00a5
Subhrajit Bhattacharya <subhrajit@gmail.com>
2015-05-28 00:31:32 -0400

    example changed

--- example/test.cpp
+++ example/test.cpp
@@ -1,5 +1,6 @@
 #include <cmath>
 #include <stdlib.h>
+#include <vector>
 #include <iostream>
 #include <thread>
 #include <chrono>
@@ -11,11 +12,13 @@
 int main(int argc, char *argv[])
 {
     sglViewProperties prop; // figure properties
-    prop.height = 400; // set properties if required
-    sglFigure myFig1 (prop); // create OpenGL figure instance with desired properties
-    myFig1.init (); // Display the figure
+    prop.height = 400; // set properties
+    prop.width = 400; 
     
     // -----------------------------------------
+    
+    sglFigure myFig1 (prop); // create OpenGL figure instance
+    myFig1.init (); // Display the figure
     
     // Add lines and store the handles to the lines
     std::vector<sglLine*> linesA;
@@ -29,18 +32,17 @@ int main(int argc, char *argv[])
     
     std::cout << "press any key. "; std::cin.get(); // Pause for keyboard input
     
-    // Create and show another window
-    sglFigure myFig2;
+    // -----------------------------------------
+    
+    sglFigure myFig2; // Create and show another figue window (uses default properties)
     myFig2.init ();
     
     // Add lines to the new window
-    std::vector<sglLine*> linesB;
-    dth = 2*PI/10;
-    for (double th=0.0; th<2*PI; th+=dth) {
-        auto pointer_to_line_object = myFig2.addObject( sglLine (cos(th),sin(th),sin(th/4.0), cos(th+dth),sin(th+dth),sin((th+dth)/4.0)) );
-        linesB.push_back (pointer_to_line_object);
-        pointer_to_line_object->width = th; // set a property for the line.
-    }
+    auto Xaxis = myFig2.addObject( sglLine (1.0,0.0,0.0, -1.0,0.0,0.0, {0.0,0.0,1.0,1.0}));
+    auto Yaxis = myFig2.addObject( sglLine (0.0,1.0,0.0, 0.0,-1.0,0.0, {0.0,0.0,1.0,1.0}));
+    auto Zaxis = myFig2.addObject( sglLine (0.0,0.0,1.0, 0.0,0.0,-1.0, {0.0,0.0,1.0,1.0}));
+    
+    // -----------------------------------------
     
     // Change colors of the lines in the first figure to create an animation
     while (true) {
